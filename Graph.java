10c96c1ae829ea4da42c1de0fdfdb01b78a1a4e1
Far <37771864+stdarkpha@users.noreply.github.com>
2023-11-02 22:52:36 +0700

    Initial commit

---
+++ Graph.java
@@ -0,0 +1,175 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+class Graph {
+    // Membuat representasi graph dengan adjacency list
+    private final HashMap<String, LinkedList<String>> adjList;
+    private int countVertex, countEdge; // Jumlah vertex dan edge
+
+    // Constructor Graph
+    public Graph() {
+        adjList = new HashMap<>();
+        countEdge = countVertex = 0;
+    }
+
+    // Method untuk menambahkan vertex dan edge
+    public void addVertex(String vertex) {
+        adjList.put(vertex, new LinkedList<>());
+        countVertex++;
+    }
+
+    // Method untuk menambahkan edge
+    public void addEdge(String src, String dst) {
+        if (adjList.containsKey(src) && adjList.containsKey(dst)) {
+            adjList.get(src).add(dst);
+            countEdge++;
+        }
+    }
+
+    // Method untuk menghitung jumlah vertex dan edge
+    public int lengthVertex() {
+        return countVertex;
+    }
+
+    public int lengthEdge() {
+        return countEdge;
+    }
+
+    public List<String> findShortestPath(String start, String end) {
+        // Inisialisasi struktur data yang dibutuhkan
+        HashMap<String, Integer> distances = new HashMap<>();
+        HashMap<String, String> previousNodes = new HashMap<>();
+        PriorityQueue<String> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(distances::get));
+        HashSet<String> visited = new HashSet<>();
+
+        // Inisialisasi jarak awal untuk semua simpul sebagai tak terbatas
+        for (String vertex : adjList.keySet()) {
+            distances.put(vertex, Integer.MAX_VALUE);
+        }
+
+        // Set jarak awal untuk simpul awal sebagai 0
+        distances.put(start, 0);
+        priorityQueue.add(start);
+
+        // Algoritma Dijkstra
+        while (!priorityQueue.isEmpty()) {
+            String current = priorityQueue.poll();
+            visited.add(current);
+
+            LinkedList<String> neighbors = adjList.get(current);
+            for (String neighbor : neighbors) {
+                if (!visited.contains(neighbor)) {
+                    int newDistance = distances.get(current) + 1; // Bobot setiap edge dianggap 1
+
+                    if (newDistance < distances.get(neighbor)) {
+                        distances.put(neighbor, newDistance);
+                        previousNodes.put(neighbor, current);
+                        priorityQueue.add(neighbor);
+                    }
+                }
+            }
+        }
+
+        // Rekonstruksi jalur terpendek dari start ke end
+        List<String> shortestPath = new ArrayList<>();
+        String current = end;
+        while (current != null) {
+            shortestPath.add(0, current);
+            current = previousNodes.get(current);
+        }
+
+        return shortestPath;
+    }
+
+    // Method untuk mencari semua rute yang mungkin dilewati dari start ke end
+    public List<List<String>> findAllPaths(String start, String end) {
+        List<List<String>> allPaths = new ArrayList<>();
+        HashSet<String> visited = new HashSet<>();
+        List<String> path = new ArrayList<>();
+
+        findAllPathsUtil(start, end, visited, path, allPaths);
+
+        return allPaths;
+    }
+
+    // Method rekursif untuk mencari semua rute yang mungkin dilewati dari start ke
+    // end
+    private void findAllPathsUtil(String current, String end, HashSet<String> visited, List<String> path,
+            List<List<String>> allPaths) {
+        visited.add(current);
+        path.add(current);
+
+        if (current.equals(end)) {
+            allPaths.add(new ArrayList<>(path));
+        } else {
+            LinkedList<String> neighbors = adjList.get(current);
+            for (String neighbor : neighbors) {
+                if (!visited.contains(neighbor)) {
+                    findAllPathsUtil(neighbor, end, visited, path, allPaths);
+                }
+            }
+        }
+
+        visited.remove(current);
+        path.remove(path.size() - 1);
+    }
+
+    // Method untuk mencetak graph
+    public void printGraph() {
+        for (String vertex : adjList.keySet()) {
+            System.out.println(vertex + ":");
+            for (String data : adjList.get(vertex))
+                System.out.print(" - " + data + " ");
+            System.out.println();
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Graph g = new Graph();
+        g.addVertex("V1");
+        g.addVertex("V2");
+        g.addVertex("V3");
+        g.addVertex("V4");
+        g.addVertex("V5");
+        g.addVertex("V6");
+        g.addVertex("V7");
+
+        g.addEdge("V2", "V3"); // 1
+        g.addEdge("V3", "V4"); // 1
+        g.addEdge("V5", "V6"); // 2
+        g.addEdge("V6", "V7"); // 3
+        g.addEdge("V1", "V2"); // 5
+        g.addEdge("V3", "V5"); // 5
+        g.addEdge("V2", "V5"); // 6
+        g.addEdge("V1", "V3"); // 7
+        g.addEdge("V5", "V7"); // 7
+        g.addEdge("V3", "V6"); // 10
+        g.addEdge("V1", "V4"); // 12
+        g.addEdge("V4", "V6"); // 13
+
+        System.out.println("Graph:");
+        g.printGraph();
+
+        System.out.print("\nTerdapat " + g.lengthVertex() + " vertex dan ");
+        System.out.println("Ada " + g.lengthEdge() + " edge");
+
+        List<List<String>> allPaths = g.findAllPaths("V1", "V7");
+        System.out.println("\nSemua rute yang mungkin dilewati dari V1 ke V7:");
+
+        for (int i = 0; i < allPaths.size(); i++) {
+            List<String> path = allPaths.get(i);
+            System.out.println("Rute " + (i + 1) + ": " + path);
+        }
+
+        List<String> shortestPath = g.findShortestPath("V1", "V7");
+        System.out.println("\nJarak terdekat dari V1 ke V7: " + (shortestPath.size() - 1));
+        System.out.println("Rute yang mungkin dilewati: " + shortestPath);
+    }
+}
---
+++ Search.java
@@ -0,0 +1,98 @@
+class Searching {
+    public static int[] data = null;
+    public static int awal, tengah, akhir, temp, count;
+
+    public static void main(String[] args) {
+        // Data yang ingin dicari
+        int cari = 10;
+
+        // Data yang akan dicari
+        data = new int[] { 4, 15, 10, 5, 0, 8 };
+
+        // Menampilkan data yang akan dicari
+        System.out.print("Data yang dicari: " + cari);
+
+        // Sorting data
+        sorting();
+
+        // Menampilkan data setelah diurutkan
+        System.out.print("\nData setelah diurutkan: ");
+        for (int x = 0; x < data.length; x++)
+            System.out.print(data[x] + " ");
+
+        // Pencarian menggunakan Binary Search
+        boolean temu = false;
+        awal = 0;
+        akhir = data.length - 1;
+        temp = 0;
+        count = 0;
+        int iterasi = 0;
+        System.out.println("\nIterasi Aw Ak Te Ni");
+
+        while (temu != true) {
+            tengah = (awal + akhir) / 2;
+            iterasi++;
+
+            if (data[tengah] == cari) {
+                System.out.print(iterasi + " ");
+                System.out.print(awal + " ");
+                System.out.print(akhir + " ");
+                System.out.print(tengah + " ");
+                System.out.print(data[tengah] + "\n");
+
+                temu = true;
+                break;
+            } else if (data[tengah] < cari) {
+                System.out.print(iterasi + " ");
+                System.out.print(awal + " ");
+                System.out.print(akhir + " ");
+                System.out.print(tengah + " ");
+                System.out.print(data[tengah] + "\n");
+                awal = tengah + 1;
+            } else if (data[tengah] > cari) {
+                System.out.print(iterasi + " ");
+                System.out.print(awal + " ");
+                System.out.print(akhir + " ");
+                System.out.print(tengah + " ");
+                System.out.print(data[tengah] + "\n");
+                akhir = tengah - 1;
+            }
+
+            // Cek worst case
+            if (temp != data[tengah])
+                temp = data[tengah];
+            else
+                count++;
+
+            // Batasan untuk worst case
+            if (count == 3)
+                break;
+        }
+
+        // Output
+        if (temu == true)
+            System.out.println(
+                    "\nData " + cari + " ditemukan pada index ke-" + tengah + "\n" + "dan iterasi ke-" + iterasi);
+        else
+            System.out.println("\nData " + cari + " tidak ditemukan");
+
+    }
+
+    // Sorting ascending
+    public static void sorting() {
+        int temp = 0;
+        for (int x = 0; x < data.length; x++) {
+            for (int y = 0; y < data.length; y++) {
+                if (x == y)
+                    continue;
+                else {
+                    if (data[x] < data[y]) {
+                        temp = data[y];
+                        data[y] = data[x];
+                        data[x] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
